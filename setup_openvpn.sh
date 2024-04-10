41187bd26754d399f7b834bf5143e02b006a023e
ctinnil <69745175+ctinnil@users.noreply.github.com>
2024-04-10 15:36:49 +0000

    v2

--- setup_openvpn.sh
+++ setup_openvpn.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
+# Default values for command line arguments
+PORT=1194
+PROTOCOL=udp
+DEV_TYPE=tun
+
+# Process command line arguments
+while getopts ":p:t:m:client:" opt; do
+  case $opt in
+    p) PORT=$OPTARG;;
+    t) PROTOCOL=$OPTARG;;
+    m) DEV_TYPE=$OPTARG;;
+    client) CLIENTS=$OPTARG;;
+    \?) echo "Invalid option -$OPTARG" >&2; exit 1;;
+  esac
+done
+
 # Ensure the script is run as root
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
    exit 1
 fi
 
-# Step 1: Update System Packages
-apt update && apt upgrade -y
+# Step 1: Install UFW and Setup Initial Rules
+apt update && apt install ufw -y
+ufw allow OpenSSH
+ufw enable
+ufw allow $PORT/$PROTOCOL
 
 # Step 2: Install OpenVPN and Easy-RSA
 apt install openvpn easy-rsa -y
@@ -18,33 +37,42 @@ cd ~/openvpn-ca
 ./easyrsa init-pki
 ./easyrsa build-ca nopass
 
-# Step 4: Create Server and Client Certificates
+# Create server certificate
 ./easyrsa gen-req server nopass
 ./easyrsa sign-req server server
 
-# Client setup
-# Change 'client' to your client's name
-./easyrsa gen-req client nopass
-./easyrsa sign-req client client
+# Function to create client certificates
+create_clients() {
+    local existing_count=$(ls pki/issued | grep 'client_' | wc -l)
+    local end=$(($existing_count + $1))
+    for (( i=$existing_count+1; i<=$end; i++ )); do
+        ./easyrsa gen-req client_$i nopass
+        ./easyrsa sign-req client client_$i
+    done
+}
 
-# Step 5: Configure the OpenVPN Server
-gunzip -c /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz > /etc/openvpn/server/server.conf
-sed -i 's/;tls-auth ta.key 0/tls-auth ta.key 0/' /etc/openvpn/server/server.conf
-sed -i 's/;cipher AES-256-CBC/cipher AES-256-CBC/' /etc/openvpn/server/server.conf
-sed -i 's/;user nobody/user nobody/' /etc/openvpn/server/server.conf
-sed -i 's/;group nogroup/group nogroup/' /etc/openvpn/server/server.conf
+# Check if script is called with client argument
+if [[ ! -z $CLIENTS ]]; then
+    create_clients $CLIENTS
+else
+    echo -n "Enter number of initial clients to create: "
+    read CLIENT_COUNT
+    create_clients $CLIENT_COUNT
+fi
 
-# Step 6: Adjust Server Networking Configuration
+# Step 4: Configure the OpenVPN Server
+gunzip -c /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz > /etc/openvpn/server.conf
+sed -i "s/port 1194/port $PORT/" /etc/openvpn/server.conf
+sed -i "s/proto udp/proto $PROTOCOL/" /etc/openvpn/server.conf
+sed -i "s/dev tun/dev $DEV_TYPE/" /etc/openvpn/server.conf
+sed -i 's/;user nobody/user nobody/' /etc/openvpn/server.conf
+sed -i 's/;group nogroup/group nogroup/' /etc/openvpn/server.conf
+
+# Step 5: Adjust Server Networking Configuration
 echo 'net.ipv4.ip_forward = 1' >> /etc/sysctl.conf
 sysctl -p
 
-# Set up firewall
-ufw allow 1194/udp
-ufw allow OpenSSH
-ufw disable
-ufw enable
-
-# Step 7: Start and Enable the OpenVPN Service
+# Start and Enable the OpenVPN Service
 systemctl start openvpn@server
 systemctl enable openvpn@server
 
